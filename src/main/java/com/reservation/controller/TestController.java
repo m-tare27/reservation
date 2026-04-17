f04630c455703a21d435eaa8f476045fdeb6ed46
m.tare <m.tare@maxxton.com>
2026-04-17 15:30:34 +0530

    Testing Commits on Github through IntelliJ

---
+++ src/main/java/com/reservation/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.reservation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/test")
+    public String testMethod(){
+        return "Test Get";
+    }
+}
