c44f643a0d97109dc7548815583bd52009c5b147
m.tare <m.tare@maxxton.com>
2026-04-22 10:58:21 +0530

    Added ExcelService with methods - saveExcelData : imports reservations from an excel sheet - exportToExcel : exports reservation to an excel sheet

--- src/main/java/com/reservation/controller/TestController.java
+++ src/main/java/com/reservation/controller/TestController.java
@@ -4,17 +4,47 @@ import com.reservation.dto.*;
 import com.reservation.entity.ReservationStatus;
 import com.reservation.service.CancellationPolicyService;
 import com.reservation.service.CancellationService;
+import com.reservation.service.ExcelService;
 import com.reservation.service.ReservationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
-    private final CancellationService service;
+    private final ExcelService service;
+
+    @GetMapping("/download")
+    public ResponseEntity<InputStreamResource> downloadExcel() throws IOException {
+        ByteArrayInputStream file = service.exportToExcel();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION , "attachment; filename=data.xlsx")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(new InputStreamResource(file));
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadExcel(@RequestParam("file") MultipartFile file) {
+        try {
+            service.saveExcelData(file);
+            return ResponseEntity.ok("File uploaded successfully");
+        } catch (Exception e) {
+            e.printStackTrace(); // VERY IMPORTANT
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(e.getMessage());
+        }
+    }
 
 //    private final ReservationService reservationService;
 //
---
+++ src/main/java/com/reservation/service/ExcelService.java
@@ -0,0 +1,112 @@
+package com.reservation.service;
+
+import com.reservation.entity.Reservation;
+import com.reservation.entity.ReservationStatus;
+import com.reservation.repository.ReservationRepository;
+import lombok.RequiredArgsConstructor;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ExcelService {
+
+    private final ReservationRepository reservationRepository;
+
+    public ByteArrayInputStream exportToExcel() throws IOException {
+
+        List<Reservation> data = reservationRepository.findAll();
+
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Data");
+
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("id");
+        header.createCell(1).setCellValue("arrival_date");
+        header.createCell(2).setCellValue("bungalow_id");
+        header.createCell(3).setCellValue("created_at");
+        header.createCell(4).setCellValue("departure_date");
+        header.createCell(5).setCellValue("guest_name");
+        header.createCell(6).setCellValue("guest_email");
+        header.createCell(7).setCellValue("reservation_status");
+        header.createCell(8).setCellValue("total_amount");
+
+        int rowIdx = 1;
+
+        for (Reservation reservation : data){
+            Row row = sheet.createRow(rowIdx++);
+
+            row.createCell(0).setCellValue(reservation.getId());
+            row.createCell(1).setCellValue(reservation.getArrivalDate().toString());
+            row.createCell(2).setCellValue(reservation.getBungalowId());
+            row.createCell(3).setCellValue(reservation.getCreatedAt().toString());
+            row.createCell(4).setCellValue(reservation.getDepartureDate().toString());
+            row.createCell(5).setCellValue(reservation.getGuestName());
+            row.createCell(6).setCellValue(reservation.getGuestEmail());
+            row.createCell(7).setCellValue(reservation.getReservationStatus().toString());
+            row.createCell(8).setCellValue(reservation.getTotalAmount());
+        }
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        workbook.write(out);
+        workbook.close();
+
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
+    public void saveExcelData(MultipartFile file) throws Exception {
+
+        Workbook workbook = new HSSFWorkbook(file.getInputStream());
+        Sheet sheet = workbook.getSheetAt(0);
+
+        List<Reservation> list = new ArrayList<>();
+
+        for (Row row : sheet) {
+            if (row.getRowNum() == 0) continue; // skip header
+
+            Reservation entity = new Reservation();
+
+            entity.setArrivalDate(
+                    LocalDate.parse(row.getCell(1).getStringCellValue())
+            );
+
+            entity.setBungalowId((int) row.getCell(2).getNumericCellValue());
+
+            entity.setCreatedAt(
+                    LocalDateTime.parse(row.getCell(3).getStringCellValue())
+            );
+
+            entity.setDepartureDate(
+                    LocalDate.parse(row.getCell(4).getStringCellValue())
+            );
+
+            entity.setGuestName(row.getCell(5).getStringCellValue());
+
+            entity.setGuestEmail(row.getCell(6).getStringCellValue());
+
+            entity.setReservationStatus(
+                    ReservationStatus.valueOf(row.getCell(7).getStringCellValue())
+            );
+
+            entity.setTotalAmount(row.getCell(8).getNumericCellValue());
+
+            list.add(entity);
+        }
+
+        reservationRepository.saveAll(list);
+        workbook.close();
+    }
+}
