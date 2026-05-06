98e100da3223e5ca5651a328b756fa6e5e24d1c5
m.tare <m.tare@maxxton.com>
2026-05-06 16:56:44 +0530

    Miscellaneous

--- src/main/java/com/reservation/ReservationApplication.java
+++ src/main/java/com/reservation/ReservationApplication.java
@@ -2,6 +2,7 @@ package com.reservation;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 description = "Comprehensive REST API for managing reservation lifecycles, including booking, payments, and cancellations."
         ),
         servers = {
-                @io.swagger.v3.oas.annotations.servers.Server(
+                @Server(
                         url = "http://localhost:8080",
                         description = "Local server"
                 )
