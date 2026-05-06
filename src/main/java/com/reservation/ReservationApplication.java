1b7cd31f45a724130c6b16dd88f61139935ce055
m.tare <m.tare@maxxton.com>
2026-05-06 14:24:56 +0530

    Improved Documentation

--- src/main/java/com/reservation/ReservationApplication.java
+++ src/main/java/com/reservation/ReservationApplication.java
@@ -1,9 +1,23 @@
 package com.reservation;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Reservation API",
+                version = "1.0",
+                description = "Comprehensive REST API for managing reservation lifecycles, including booking, payments, and cancellations."
+        ),
+        servers = {
+                @io.swagger.v3.oas.annotations.servers.Server(
+                        url = "http://localhost:8080",
+                        description = "Local server"
+                )
+        }
+)
 @SpringBootApplication
 @EnableScheduling
 public class ReservationApplication {
